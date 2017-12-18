459f20a338718a41e49e180e0b2aff748d37d33f
HuuDuc <haleluak@gmail.com>
2017-12-18 15:40:07 +0700

    add key

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$APP_ID = ""; // YOUR APP ID
-$APP_SECRET = ""; // YOUR APP PASSWORD
+$APP_ID = "3eec3fed-0de5-43d6-b982-70244d8bcff8"; // YOUR APP ID
+$APP_SECRET = "ijojABJ2448:%wrpZPGC9~@"; // YOUR APP PASSWORD
 $TOKEN_FILE = "token.json";
 ?>
