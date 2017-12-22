5eabcf70c0ac7abefa2c87fcb950e865857e70d7
HuuDuc <haleluak@gmail.com>
2017-12-22 10:00:56 +0700

    format price

--- function.php
+++ function.php
@@ -129,8 +129,7 @@ function response()
 		$url = 'https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-'. $coin ; // path to your JSON file
 		$data = file_get_contents($url); // put the contents of the file into a variable
 		$characters = json_decode($data); // decode the JSON feed
-		//$res["text"] = 'Last price : ' . sprintf("%.8f", $characters->result[0]->Last) . ' <br /> Hight price: ' . sprintf("%.8f", $characters->result[0]->High) . ' <br /> Low price: ' . sprintf("%.8f", $characters->result[0]->Low) . ' <br /> BaseVolume: ' . $characters->result[0]->BaseVolume . ' BTC ';
-		$res["text"] = $coin;
+		$res["text"] = 'Last price : ' . sprintf("%.8f", $characters->result[0]->Last) . ' <br /> Hight price: ' . sprintf("%.8f", $characters->result[0]->High) . ' <br /> Low price: ' . sprintf("%.8f", $characters->result[0]->Low) . ' <br /> BaseVolume: ' . $characters->result[0]->BaseVolume . ' BTC ';
 		reply($req, $res);
 	}
 }
