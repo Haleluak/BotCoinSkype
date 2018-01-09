e7703cc31e10395f4fdb09dae80eff0d6777f3f2
HuuDuc <haleluak@gmail.com>
2018-01-09 14:49:40 +0700

    update bot

--- function.php
+++ function.php
@@ -179,13 +179,6 @@ function response()
 		else
 		{
 			$res["text"] = coinmarketcap($coin);
-			$res["attachments"] = [
-				{
-					"contentType": "image/png",
-					"contentUrl": "http://sohanews.sohacdn.com/2013/images-1376406875371.jpg",
-					"name": "troll.jpg"
-				}
-			];
 		}
 		reply($req, $res);
 	}
