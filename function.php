5e24a2ec042c424217b57a927899847c6692e8b1
HuuDuc <haleluak@gmail.com>
2018-01-12 15:36:11 +0700

    add image

--- function.php
+++ function.php
@@ -186,7 +186,7 @@ function response()
 		}
 		else
 		{
-			$res["text"] = coinmarketcap($coin);	
+			$res["text"] = coinmarketcap($coin);
 			/*$res['attachments'] = array(
 					"contentType": "image/jpg",
 					"contentUrl": "http://ice-apple.com/public/upload/avatars/citizen/27866.jpg",
